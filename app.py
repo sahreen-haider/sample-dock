74838021246cb5a69616a6c08567edfb84fde1c7
sahreen-haider <sahreenhaider@gmail.com>
2023-11-25 16:09:23 +0530

    further changes in app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/',  method=['GET'])
+@app.route('/')
 def welcome():
-    return render_template('index.html')
-    # return '<h>Hello, Welcome!</h>' 
+    return '<h>Hello, Welcome!</h>' 
     
 
 if __name__ == '__main__':
