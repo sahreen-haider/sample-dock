ca105812250efecc76c252e0433ffa813655922a
sahreen-haider <sahreenhaider@gmail.com>
2023-11-27 17:04:30 +0530

    changes to the app.py and template

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return '<h>Hello, Welcome!</h>' 
+    return render_template('index.html')
+
+@app.route('/predict')
+def index():
+    pass
+
     
 
 if __name__ == '__main__':
